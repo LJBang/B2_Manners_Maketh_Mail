d8824aa439875fa4e8447b8c82605bf531766855
LJBang <kailo123457@gmail.com>
2022-04-26 18:59:36 +0900

    Mod: Site Layout

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-#from crypt import methods
 from flask import Flask, request, render_template
 import model
 
@@ -6,15 +5,16 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/main', methods=['GET', 'POST'])
 def main(sent=None):
-    if request.method == 'GET':
-        return render_template('main.html', sent=sent)
-    elif request.method == 'POST':
-        sent = request.form['sent']
-        print('==============')
-        print(sent)
-        print('==============')
-        reco = model.recommand(sent)
-        return render_template('main.html', sent=sent, reco=reco)
-
+    try:
+        if request.method == 'GET':
+            return render_template('main.html', sent=sent)
+        elif request.method == 'POST':
+            sent = request.form['sent']
+            if sent == '':
+                return render_template('main.html', sent=None)
+            reco1, reco2, reco3 = model.recommand(sent)
+            return render_template('main.html', sent=sent, reco1=reco1, reco2=reco2, reco3=reco3)
+    except:
+        return render_template('main.html', sent=None)
 if __name__ == '__main__':
     app.run(debug=True)
